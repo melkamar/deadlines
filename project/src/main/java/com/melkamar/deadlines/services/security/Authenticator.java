3224f9d084bac37f4ffa792af5a7f796534f2679
Martin <martin.melka@gmail.com>
2016-03-31 12:06:17 +0200

    Add Authenticator service

---
+++ project/src/main/java/com/melkamar/deadlines/services/security/Authenticator.java
@@ -0,0 +1,32 @@
+package com.melkamar.deadlines.services.security;
+
+import com.melkamar.deadlines.dao.user.UserDAO;
+import com.melkamar.deadlines.dao.user.UserDAOHibernate;
+import com.melkamar.deadlines.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Martin Melka (martin.melka@gmail.com)
+ * 31.03.2016 11:45
+ */
+@Service
+public class Authenticator {
+    @Autowired
+    private ShaPasswordEncoder passwordEncoder;
+
+    /**
+     * Authenticates the given user by with his password.
+     * If the authentication failed, returns null.
+     * If it succeeded, returns the user object.
+     */
+    public User authenticate(User user, String password){
+        String encoded = passwordEncoder.encodePassword(password, user.getPasswordSalt());
+        if (encoded.equals(user.getPasswordHash())){
+            return user;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ project/src/test/java/com/melkamar/deadlines/services/security/AuthenticatorTest.java
@@ -0,0 +1,44 @@
+package com.melkamar.deadlines.services.security;
+
+import com.melkamar.deadlines.DeadlinesApplication;
+import com.melkamar.deadlines.model.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Martin Melka (martin.melka@gmail.com)
+ * 31.03.2016 11:48
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DeadlinesApplication.class)
+public class AuthenticatorTest {
+    User mockedUser = Mockito.mock(User.class);
+    @Autowired
+    private Authenticator authenticator;
+
+    @Before
+    public void setUp() throws Exception {
+        Mockito.when(mockedUser.getUsername()).thenReturn("User2");
+        Mockito.when(mockedUser.getPasswordHash()).thenReturn("375c3858a8d203ac6393d948098f797d7d871e32");
+        Mockito.when(mockedUser.getPasswordSalt()).thenReturn("c7badfd9725a9f2e");
+    }
+
+    @Transactional
+    @Test
+    public void authenticate() throws Exception {
+        String correctPassword = "password";
+        String wrongPassword = "abcd";
+
+        assertNull(authenticator.authenticate(mockedUser, wrongPassword));
+        assertEquals(authenticator.authenticate(mockedUser, correctPassword), mockedUser);
+    }
+}
